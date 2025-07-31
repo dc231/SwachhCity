6b743b59b8c39a4e6bb1edd0f900e24da1b77500
Dheeraj Chaudhary <dheerajchaudhary231@gmail.com>
2025-07-31 22:51:21 +0530

    add state management to login form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 
 function Login() {
+    const [inputValue, setInputValue] = useState({
+        email: '',
+        password: '',
+    });
+
+    const { email, password } = inputValue;
+
+    const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setInputValue({
+        ...inputValue,
+        [name]: value,
+     });
+    };
   return (
     <div className="container mt-5" style={{ maxWidth: '500px' }}>
       <h2>Login</h2>
@@ -12,7 +27,10 @@ function Login() {
             type="email"
             className="form-control"
             id="email"
+            name="email"
+            value={email}
             placeholder="Enter your email"
+            onChange={handleOnChange}
           />
         </div>
         <div className="mb-3">
@@ -21,7 +39,10 @@ function Login() {
             type="password"
             className="form-control"
             id="password"
+            name="password"
+            value={password}
             placeholder="Enter your password"
+            onChange={handleOnChange}
           />
         </div>
         <button type="submit" className="btn btn-primary">Login</button>
