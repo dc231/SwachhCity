378bddf03a504f9187a77bc868b04cc126a8adc3
Dheeraj Chaudhary <dheerajchaudhary231@gmail.com>
2025-07-31 11:12:44 +0530

    feat: add admin signup functionality

--- backend/Routes/AuthRoute.js
+++ backend/Routes/AuthRoute.js
@@ -1,7 +1,8 @@
-const { Signup, Login } = require('../Controllers/AuthController');
+const { Signup, Login, AdminSignup } = require('../Controllers/AuthController');
 const router = require('express').Router();
 
 router.post('/signup', Signup);
 router.post('/login', Login);
+router.post('/adminsignup', AdminSignup);
 
 module.exports = router;
